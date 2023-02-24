f7886f7bf26ab26fccf2c852da44a18fc73a21e7
Martin <mininnov99@kaist.ac.kr>
2023-02-24 22:15:54 +0900

    GA setting test

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -9,18 +9,34 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css' />
+       <Head>
+          {/* GTM code snippet */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-P92NZ3R');
+              `,
+            }}
+          />
+          {/* End GTM code snippet */}
         </Head>
         <body>
-            <Main />
-            <NextScript />
-            <noscript
-                dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P92NZ3R"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-                }}
+          {/* GTM body code */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P92NZ3R"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
             />
+          </noscript>
+          {/* End GTM body code */}
+          <Main />
+          <NextScript />
         </body>
       </Html>
     )
