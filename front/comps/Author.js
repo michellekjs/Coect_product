39307dcdc51383ef25a93c26309118b83964c8f7
michelle <michellekjs@kaist.ac.kr>
2023-02-28 01:57:05 +0900

    change img tags into nextjs Image tags

--- front/comps/Author.js
+++ front/comps/Author.js
@@ -1,30 +1,47 @@
-import { colors } from '../shared'
+import { colors } from "../shared";
+import Image from "next/image";
 
 export default function Author(props) {
-    const styles = {
-        author: {
-            display: 'flex', alignItems: 'center', gap: props.verySmall ? 6 : props.small ? 4 : 6,
-        },
-        authorImage: {
-            borderRadius: '50%',
-            width: props.verySmall ? 17 : props.small ? 20 : 30, height: props.verySmall ? 17 : props.small ? 20 : 30,
-        },
-        authorName: {
-            fontWeight: 'normal',
-            display: '-webkit-box',
-            WebkitBoxOrient: 'vertical',
-            WebkitLineClamp: 1,
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            fontSize : props.verySmall ? 12 : props.small ? 14 : 16,
-            color: colors._300
-        },
-    }
+	const myLoader = ({ src }) => {
+		return src;
+	};
 
-    return (
-        <div style={styles.author}>
-            <img style={styles.authorImage} src={props.image || "https://via.placeholder.com/30x30"}/>
-            <div style={styles.authorName}>{props.name || 'UNDEFINED'}</div>
-        </div>
-    )
-};
+	const styles = {
+		author: {
+			display: "flex",
+			alignItems: "center",
+			gap: props.verySmall ? 6 : props.small ? 4 : 6,
+		},
+		authorImage: {
+			borderRadius: "50%",
+			width: props.verySmall ? 17 : props.small ? 20 : 30,
+			height: props.verySmall ? 17 : props.small ? 20 : 30,
+		},
+		authorName: {
+			fontWeight: "normal",
+			display: "-webkit-box",
+			WebkitBoxOrient: "vertical",
+			WebkitLineClamp: 1,
+			overflow: "hidden",
+			textOverflow: "ellipsis",
+			fontSize: props.verySmall ? 12 : props.small ? 14 : 16,
+			color: colors._300,
+		},
+	};
+
+	return (
+		<div style={styles.author}>
+			{/* <img style={styles.authorImage} src={props.image || "https://via.placeholder.com/30x30"}/> */}
+			<Image
+				style={styles.authorImage}
+                loader={myLoader}
+				src={props.image}
+				alt="https://via.placeholder.com/30x30"
+				width={30}
+				height={30}
+				
+			/>
+			<div style={styles.authorName}>{props.name || "UNDEFINED"}</div>
+		</div>
+	);
+}
